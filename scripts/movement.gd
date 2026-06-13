8591a4b2b2da3e2b32910c7e1feeb228a6ad91ce
Lizzy1640 <lizzy.gillz05@gmail.com>
2026-06-12 19:15:10 -0700

    Commented out faulty code.
    Just to get rid of error messages.

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -7,6 +7,6 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	# adding the gravity
-	if not is_on_floor():
+	#if not is_on_floor():
 		position.x += SPEED * delta
-		velocity.y += gravity * delta
+		#velocity.y += gravity * delta

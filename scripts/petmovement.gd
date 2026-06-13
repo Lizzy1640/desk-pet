ccbdb68bb2ce87f2709662a61e231afe513ba3af
Lizzy1640 <lizzy.gillz05@gmail.com>
2026-06-12 21:29:01 -0700

    Starting to implement click and drag
    Attempted to add click and drag, but am now watching tutorials.

--- scripts/petmovement.gd
+++ scripts/petmovement.gd
@@ -1,25 +1,41 @@
 extends CharacterBody2D
 
 
-const SPEED = 60.0
-var direction = 1
-#const JUMP_VELOCITY = -400.0
+const SPEED := 60.0 # float
+var direction := 1 #int
+var is_dragging := false # boolean
+var initial_posit := Vector2.ZERO # idk tbh
+
 @onready var collider = $CollisionShape2D
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	_fall(delta)
+	_walk()
+	
+
+func _walk():
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+
+func _fall(delta: float):
+	# need to get the x and y velocity of mouse
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	position.x += direction * SPEED * delta
-	
-	if ray_cast_right.is_colliding():
-		direction = -1
-	if ray_cast_left.is_colliding():
-		direction = 1
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
+
+func _click_and_drag(event: InputEvent) -> void:
+	#if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		#if event.is_pressed():
+			
+		#elif event.is_released():
+			
 
 	move_and_slide()
